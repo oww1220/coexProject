710a780b73fc254779bad0c614b635201e627b7d
이동걸 <0193100266@naver.com>
2019-07-14 14:25:38 +0900

    20190714 코딩본

--- js/common.js
+++ js/common.js
@@ -1,26 +1,59 @@
-$(document).ready(function(){
-	$("#navBtnOpen").click(function(){
-		$("#localMenuBox").addClass("on");
-		$(".dimmed").addClass("on");
-	});
-	$("#navBtnClose").click(function(){
-		$("#localMenuBox").removeClass("on");
-		$(".dimmed").removeClass("on");
+var COEX = COEX || {
+	resize: {
+		chk: function(target){
+			if (target.width() >= 1024 ) {
+				target.removeClass("pc mobile tablet").addClass("pc");
+			}
+			else if(target.width() >=  768){
+				target.removeClass("pc mobile tablet").addClass("tablet");
+			}
+			else {
+				target.removeClass("pc mobile tablet").addClass("mobile");
+			}
+		},
+		pf: function(target){
+			var pf = navigator.platform;
+			if(/iphone/i.test(pf)){ //아이폰일때
+				target.addClass("iphone");
+			}
+		}
+	},
+	event: {
+		toggle: function(target){
+			var layer = $(target.data("target"));
+			target.on("click", function(){
+				if(layer.is(":visible")){
+					layer.slideUp();
+				}
+				else{
+					layer.slideDown();
+				}
+			});
+		},
+	}
+};
+
+
+$(function(){
+
+	var BODY = $("body");
+
+	/*호스트환경 체크*/
+	COEX.resize.pf(BODY);
+	COEX.resize.chk(BODY);
+
+	/*푸터 토글*/
+	COEX.event.toggle($(".footer .family_wrap .family_btn"));
+
+	$(window).on("load", function(){
 	});
 
-	$(".portList > li > a.none").click(function(){
-		$(this).parents("li").addClass("on").siblings().removeClass("on");
+	$(window).on("resize", function(){
+		/*호스트환경 체크*/
+		COEX.resize.chk(BODY);
+
 	});
-	$(".portList > li > a.active").click(function(){
-		$(this).parents("li").removeClass("on");
-	});
+
+
 });
 
-
-/*$(document).on('click', function (e) {
-	if(!$(e.target).is("#navBtn, #navBtn span")){
-		$("#navBtn").removeClass("on");
-	} else {
-		$("#navBtn").toggleClass("on");
-	}
-});*/
