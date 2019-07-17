3d6ef2208ed7f2a8fb6d61a2316cadc240cdc05b
이동걸 <0193100266@naver.com>
2019-07-17 20:46:47 +0900

    20190717 소스수정

--- js/common.js
+++ js/common.js
@@ -16,7 +16,58 @@ var COEX = COEX || {
 			if(/iphone/i.test(pf)){ //아이폰일때
 				target.addClass("iphone");
 			}
-		}
+		},
+	},
+	slide: {
+		init: function(target, sort, option){
+			if(sort == "slick") {
+				//console.log("slick");
+				target.slick(option);
+			}
+		},
+	},
+	layer: {
+		scrollTop: 0,
+		init: function(openBt, closeBt, dimmed, parent){
+			var that = this;
+			
+			if(openBt) that.open(openBt, dimmed, parent);
+			if(closeBt) that.close(closeBt, dimmed, parent);
+			if(dimmed) that.close(dimmed, dimmed, parent);
+		},
+		open: function(target, dimmed, parent){
+			var that = this;
+			$(document).on("click", target, function(e){
+				var layer = $(this).data("layer");
+				that.scrollTop = $(window).scrollTop();
+				
+				if(dimmed) $(dimmed).fadeIn();
+				$(parent + "." + layer).show();
+				//console.log(layer, that.scrollTop);
+
+				e.preventDefault();
+			});
+		},
+		close: function(target, dimmed, parent){
+			var that = this;
+			$(document).on("click", target, function(e){
+				var layer;
+				if(target == dimmed){
+					layer = $(parent);
+					//console.log("dimmed");
+				}
+				else{
+					layer = $(parent + "."+$(this).data("layer"));
+				}
+				
+				layer.hide();
+
+				if(dimmed) $(dimmed).fadeOut();
+				//console.log(layer, that.scrollTop);
+
+				e.preventDefault();
+			});
+		},
 	},
 	event: {
 		toggle: function(target){
@@ -33,7 +84,7 @@ var COEX = COEX || {
 		goTop: function(target){
 			target.on("click",function(e) {
 				$("html, body").stop().animate({"scrollTop": 0}, 1000);
-            	e.preventDefault()
+            	e.preventDefault();
 			});
 		}, 
 		topScrollCh: function(target, parent){
@@ -50,39 +101,79 @@ var COEX = COEX || {
 				return;
 			}
 		},	
+		tap: function(tab_nav, tab_cont){
+			$(document).on("click", tab_nav, function(e){
+				var parent = $(this).parent().parent().data("tab");
+				var layer = (tab_cont + "." + parent + " > div");
+				var idx = $(this).index();
+
+				$(layer).eq(idx).show().siblings().hide();
+				//console.log(layer, idx);
+				e.preventDefault();
+			});
+		},
+		
 	},
 };
 
 
 $(function(){
 
-	var BODY = $("body");
-	var GOTOP = $(".footer .btnTop")
+	/*제이쿼리 객체 캐쉬 및 상수*/
+	var $BODY = $("body"),
+		$GOTOP = $(".footer .btnTop"),
+		$NOTICE = $(".main_wrap .cols_notice ul"),
+		$FOOTER_TOGGLE = $(".footer .family_wrap .family_btn"),
+		LAYER_BT_OPEN = ".layer_open_bt",
+		LAYER_BT_CLOSE = ".layer_close_bt",
+		LAYER_DIM = ".layer_dimmed",
+		LAYER_DIV = ".pop_layer",
+		TAB_NAV = ".tab_nav li",
+		TAB_CONTS= ".tab_cont";
+
+
 
 	/*호스트환경 체크*/
-	COEX.resize.pf(BODY);
-	COEX.resize.chk(BODY);
+	COEX.resize.pf($BODY);
+	COEX.resize.chk($BODY);
+
+
+	/*메인 공지사항 슬라이드*/
+	if($NOTICE && $.fn.slick) {
+		COEX.slide.init($NOTICE, "slick", {
+			infinite: true,
+			autoplay: true,
+			arrows: false,
+			vertical: true,
+		});
+	}
 
 	/*푸터 토글*/
-	COEX.event.toggle($(".footer .family_wrap .family_btn"));
+	COEX.event.toggle($FOOTER_TOGGLE);
 
 	/*pc top으로 scroll*/
-	COEX.event.topScrollCh(GOTOP, BODY);
+	COEX.event.topScrollCh($GOTOP, $BODY);
 
 	/*top으로*/
-	COEX.event.goTop(GOTOP);
+	COEX.event.goTop($GOTOP);
+
+	/*layer팝업*/
+	COEX.layer.init(LAYER_BT_OPEN, LAYER_BT_CLOSE, LAYER_DIM, LAYER_DIV);
+
+	/*탭버튼*/
+	COEX.event.tap(TAB_NAV, TAB_CONTS);
 
 	$(window).on("load", function(){
 	});
 
 	$(window).on("scroll", function(){
 		/*pc top으로 scroll*/
-		COEX.event.topScrollCh(GOTOP, BODY);
+		COEX.event.topScrollCh($GOTOP, $BODY);
 	});
 
 	$(window).on("resize", function(){
 		/*호스트환경 체크*/
-		COEX.resize.chk(BODY);
+		COEX.resize.chk($BODY);
 
 	});
 
